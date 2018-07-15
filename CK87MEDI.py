458a3c56a87f083c1053eda15fa3540800654247
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-07-15 17:06:47 +0530

    Solution to the CodeChef problem CK87MEDI
    Link to the problem - https://www.codechef.com/problems/CK87MEDI

---
+++ CK87MEDI.py
@@ -0,0 +1,10 @@
+for i in range(int(input())):
+    n,k=[int(x) for x in input().split()]
+    median=(n+k+1)//2
+    l=[int(x) for x in input().split()]
+    length=len(l)
+    if length>=median:
+        l.sort()
+        print(l[median-1])
+    else:
+        print(1000)
