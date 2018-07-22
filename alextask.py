b3ef7d464868be6e5e27a5b18ff2b42593793e3c
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-07-23 00:04:56 +0530

    Solution to the CodeChef problem ALEXTASK
    Link to the problem - https://www.codechef.com/NOV16/problems/ALEXTASK

---
+++ alextask.py
@@ -0,0 +1,24 @@
+def gcd(a, b):
+    if a == 0:
+        return b
+    else:
+        return(gcd(b % a, a))
+
+def lcm(a, b):
+    return((a * b)/gcd(a, b))
+
+for i in range(int(input())):
+    n = int(input())
+
+    a = [int(x) for x in input().split()]
+
+    mn = lcm(a[0], a[1])
+
+    for i in range(n):
+        for j in range(1, n):
+            if i == j:
+                continue
+
+            mn = min(lcm(a[i], a[j]), mn)
+
+    print(int(mn))
