6662efea6d5c87457b3b119bb9644fb778866ab1
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-07-30 02:02:35 +0530

    Solution to the CodeChef problem STICKS
    Link to the problem - https://www.codechef.com/problems/STICKS

---
+++ sticks.py
@@ -0,0 +1,23 @@
+for i in range(int(input())):
+    n=int(input())
+    size=[int(x) for x in input().split()]
+    s=set(size)
+    s=list(s)
+    s.sort()
+    area=1
+    c=0
+    for i in s[::-1]:
+        if size.count(i)>=2 and c<2:
+            if size.count(i)>=4 and c==0:
+                c=2
+                area=area*i**2
+                break
+            else:
+                c+=1
+                area*=i
+        if c>2:
+            break
+    if c==2:
+        print(area)
+    else:
+        print(-1)
