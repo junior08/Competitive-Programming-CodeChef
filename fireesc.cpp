bb5b5de443cc6f0667db9b608c3f2aba188c5882
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-07-30 19:30:48 +0530

    Solution to the CodeChef problem FIRESC
    Link to the problem - https://www.codechef.com/problems/FIRESC

---
+++ fireesc.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+long long int par[100005];
+long long int s[100005];
+ 
+int find(int x){
+	return par[x]=(par[x]==x)?x:find(par[x]);
+}
+ 
+int merge(int x, int y){
+	if(x > y)
+		swap(x, y);
+	par[find(x)] = find(y);
+	/*if(find(x)<find(y)){
+		par[find(y)]=find(x);
+	}
+	else{
+		par[find(x)]=find(y);
+	}*/
+}
+ 
+int main(){
+	int t, n, m, a, b;
+	long long int leaders;
+	long long int num_of_sets;
+	std::ios_base::sync_with_stdio(false);
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		leaders = 1;
+		num_of_sets = 0;
+		for(int i=1;i<=n;i++){
+			par[i]=i;
+			s[i]=0;
+		}
+		
+		for(int i = 1;i <= m;i++){
+			cin>>a>>b;
+			merge(a,b);
+		}
+
+		for(int i = 1;i <= n;i++){
+			s[find(i)] += 1;
+			if(find(i) == i){
+				num_of_sets++;
+			}
+		}
+		
+		for(int i = 1;i <= n;i++){
+			if(s[i]>0)
+				leaders *= s[i];
+			leaders %= 1000000007;
+		}
+		cout<<num_of_sets<<" "<<leaders<<endl;
+	}
+}  
