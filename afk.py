9cf3016f3abfd1a304ba884f22f216289b5c4689
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-09-26 07:03:58 +0530

    Solution to AFK

---
+++ afk.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    a, b, c = [int(x) for x in input().split()]
+    if (a + c) % 2 == 1:
+        ans1 = abs(b - ((a + c -1) // 2))
+        ans2 = abs(b - ((a + c + 1) // 2))
+        print(min(ans1, ans2) + 1)
+    else:
+        print(abs(b - (a + c)//2))
