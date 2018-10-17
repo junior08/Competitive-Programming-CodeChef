b2d427d63a2b8197ebd0a4b96bbc6d235d3af04d
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-10-17 17:24:09 +0530

    Update chbllns.py

--- chbllns.py
+++ chbllns.py
@@ -1,10 +1,10 @@
-for i in range(int(input())):
-    r, g, b = [int(x) for x in input().split()]
+for i in range(int(input())):#Run for the given number of test cases
+    r, g, b = [int(x) for x in input().split()]#Take input
     k = int(input())
     ans = 0
 
     if k == 1:
         print(1)
         continue
-    print(min(r, k - 1) + min(g, k - 1) + min(b, k - 1) + 1)
+    print(min(r, k - 1) + min(g, k - 1) + min(b, k - 1) + 1)#Display the answer
         
