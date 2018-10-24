031127091f8239b7db4a4331a6b8865f952220dc
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-10-24 12:30:09 +0530

    Solution to MMSUM

---
+++ mmsum.py
@@ -0,0 +1,17 @@
+for i in range(int(input())):
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    max_all = 0
+    max_skip = 0
+    flag = 0
+    ans = 0
+    
+    for i in a:
+        if i >= 0:
+            flag = 1
+        max_all, max_skip = max(max_all + i, 0),max(0, max_all, max_skip + i)
+        ans = max(ans, max_all, max_skip)
+    if flag == 1:
+        print(ans)
+    else:
+        print(max(a))
