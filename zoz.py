2eb31cb4a8f5b4902dc5eb822c1303a9d8f2a691
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-02 02:38:43 +0530

    Update zoz.py

--- zoz.py
+++ zoz.py
@@ -1,10 +1,10 @@
-for i in range(int(input())):
-    n, k = [int(s) for s in input().split()]
-    arr = [int(x) for x in input().split()]
-    sm = sum(arr)
+for i in range(int(input())): # i = number of test cases
+    n, k = [int(s) for s in input().split()] # n = size of array A, k = given number
+    arr = [int(x) for x in input().split()] # arr = array
+    sm = sum(arr) # sum of the elements of the array
     valid = 0
 
     for i in range(n):
-        if arr[i] + k > sm - arr[i]:
+        if arr[i] + k > sm - arr[i]: # checking if the position is valid
             valid += 1
-    print(valid)
+    print(valid) # printing number of valid positions
