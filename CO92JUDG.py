6639e3f68d9d209f06b0da850f52b0041161e9a8
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-04 00:54:01 +0530

    Update CO92JUDG.py

--- CO92JUDG.py
+++ CO92JUDG.py
@@ -1,14 +1,14 @@
-for i in range(int(input())):
-    n = int(input())
-    a = [int(x) for x in input().split()]
-    b = [int(x) for x in input().split()]
+for i in range(int(input())): # number of test cases
+    n = int(input()) # number of races
+    a = [int(x) for x in input().split()] # time taken by alice to finish each x-th race
+    b = [int(x) for x in input().split()] # time taken by bob to finish each x-th race
     a.sort()
     b.sort()
     asum = sum(a) - a[-1]
     bsum = sum(b) - b[-1]
     if asum > bsum:
-        print("Bob")
+        print("Bob") # Bob wins
     elif asum < bsum:
-        print("Alice")
+        print("Alice") # Alice wins
     else:
-        print("Draw")
+        print("Draw") # draw
