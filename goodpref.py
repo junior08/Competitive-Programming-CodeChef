87bbd6a321cef15e6138aae5b2c7f9359a15e701
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-06 12:51:57 +0530

    Update goodpref.py

--- goodpref.py
+++ goodpref.py
@@ -1,4 +1,4 @@
-def pref(s):
+def pref(s): # calculates the number of occurrences of ('a' > 'b')
     ans = 0
     ac, bc = 0, 0
     for i in s: 
@@ -9,14 +9,14 @@ def pref(s):
         if bc < ac:
             ans += 1
     return ans
-for i in range(int(input())):
-    s, n = input().split()
+for i in range(int(input())): # number of test cases
+    s, n = input().split() # given string s and integer n
     n = int(n)
 ##    print(pref(s * n))
-    a = s.count('a')
-    b = s.count('b')
+    a = s.count('a') # count number of 'a' in string s
+    b = s.count('b') # count number of 'b' in string s
     ans1 = pref(s)
-    ans2 = pref(s * min(1000, n))
+    ans2 = pref(s * min(1000, n)) 
     if pref(s) == 0:
         print(0)
         continue
