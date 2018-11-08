eebefcefab7e26161b59b1fc4149f1a6137490c9
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-08 18:29:15 +0530

    Update chchcl.py

--- chchcl.py
+++ chchcl.py
@@ -1,6 +1,6 @@
-for i in range(int(input())):
-    n,m=[int(x) for x in input().split()]
+for i in range(int(input())): # number of test cases
+    n,m=[int(x) for x in input().split()] # n = rows, m = columns of the chocolate
     if (n*m)%2==0:
-        print("Yes")
+        print("Yes") # the person who moves first wins
     else:
-        print("No")
+        print("No") # otherwise
