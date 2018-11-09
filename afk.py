72e5af5332cd5a68af773234798a33c7c7397587
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-09 11:19:49 +0530

    Update afk.py

--- afk.py
+++ afk.py
@@ -3,6 +3,6 @@ for i in range(int(input())):# Accept input
     if (a + c) % 2 == 1:
         ans1 = abs(b - ((a + c -1) // 2))
         ans2 = abs(b - ((a + c + 1) // 2))
-        print(min(ans1, ans2) + 1)
+        print(min(ans1, ans2) + 1)#print answer
     else:
         print(abs(b - (a + c)//2))
