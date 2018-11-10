d0fb7892ef463ac6d88110a739be6df11b641a8e
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-10 18:18:34 +0530

    Update kttable.py

--- kttable.py
+++ kttable.py
@@ -1,12 +1,12 @@
-for i in range(int(input())):
-    n=int(input())
-    l=[int(x) for x in input().split()]
+for i in range(int(input())): # number of test cases
+    n=int(input()) # number of students
+    l=[int(x) for x in input().split()] # time at which students should finish cooking
     l2=[]
     l2.append(l[0])
     k=0
     for c in range(1,len(l)):
         l2.append(l[c]-l[c-1])
-    l3=[int(x) for x in input().split()]
+    l3=[int(x) for x in input().split()] # time required for each student to cook
     for i,j in zip(l2,l3):
         if j<=i:
             k+=1
