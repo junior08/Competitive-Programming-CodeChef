5c09b5438cf26d6637b4db580819c8a5b3e3878c
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-11-21 19:51:35 +0530

    Update cielab.py

--- cielab.py
+++ cielab.py
@@ -1,6 +1,6 @@
-l=[int(x) for x in input().split()]
-x= l[0]-l[1]
+l=[int(x) for x in input().split()] # accept input A and B in list l
+x= l[0]-l[1] # difference between A and B
 if str(x)[0]!='4':
-    print('4'+str(x)[1:])
+    print('4'+str(x)[1:]) 
 else:
     print('5'+str(x)[1:])
