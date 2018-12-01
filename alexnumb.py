6b1f213d0dcae4cb413007042ee23c8d652d3f44
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-12-01 21:47:06 +0530

    Update alexnumb.py

--- alexnumb.py
+++ alexnumb.py
@@ -1,4 +1,4 @@
 for i in range(int(input())):#Run for t test cases
     n = int(input()) #Take the input
-    a = [int(x) for x in input().split()]#Get the array as input
+    a = [int(x) for x in input().split()]#Get the array as input and then
     print((n * (n - 1)) // 2)#print the answer
