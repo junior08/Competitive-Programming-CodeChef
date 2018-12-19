3b9c2050b0b7a6e46749d22dd0ade1d20b7d70fd
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-12-19 22:46:23 +0530

    Update anuprod.py

--- anuprod.py
+++ anuprod.py
@@ -1,8 +1,8 @@
 mod = 10 ** 9 + 7
 
-n = int(input())
+n = int(input())#for number of test cases
 a = [int(c) for c in input().split()]
 ans = 1
 for i in a:
     ans = ((ans % mod) * (i % mod)) % mod
-print(ans % mod)
+print(ans % mod)#printing answer
