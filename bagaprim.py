93c6b49f01450e96c142d4c31df7606c5ebb6490
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2018-12-23 17:39:23 +0530

    Add files via upload

---
+++ bagaprim.py
@@ -0,0 +1,20 @@
+prime = []
+def Sieve(n):
+    global prime
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] == True):
+            for i in range(p * 2, n+1, p):
+                prime[i] = False
+        p += 1
+    prime[0], prime[1] = False, False
+    
+Sieve(1000000)
+for i in range(int(input())):
+    l, r = [int(x) for x in input().split()]
+    ans = 0
+    for i in range(l, r + 1):
+        if prime[i]:
+            ans += 1
+    print(ans)
