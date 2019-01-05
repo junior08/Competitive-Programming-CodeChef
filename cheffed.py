af0ac9549ad92bb1558f064f059606d75a18b1e2
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-01-05 21:59:33 +0530

    Update cheffed.py

--- cheffed.py
+++ cheffed.py
@@ -1,5 +1,5 @@
-n = int(input())
-def digsum(n):
+n = int(input())#for the given number of test cases
+def digsum(n):#find the digit sum
    r = 0
    while n:
        r, n = r + n % 10, n // 10
@@ -11,7 +11,7 @@ for i in range(max(n - 98, 0), n):
     fs = digsum(curr)
     ss = digsum(fs)
 
-    if (curr + fs + ss) == n:
+    if (curr + fs + ss) == n:#necessary condition
         count += 1
 
-print(count)
+print(count)#print the answer
