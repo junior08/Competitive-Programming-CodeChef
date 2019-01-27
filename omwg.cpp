da5e5b9d1bbe2730f750eb5593d76311488a2c1a
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-01-27 10:28:50 +0530

    Add files via upload

---
+++ omwg.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t, n, m;
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
+		int ans = 2 *(n*m) - (n + m);
+		cout<<ans<<endl;
+		
+	}
+	
+}
