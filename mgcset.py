4f925c00e776c0f03bf6de27b1b12798b02e075e
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-01-29 21:42:31 +0530

    Update mgcset.py

--- mgcset.py
+++ mgcset.py
@@ -1,11 +1,11 @@
-for i in range(int(input())):
-    n, m = [int(x) for x in input().split()]
+for i in range(int(input())):#for each test case
+    n, m = [int(x) for x in input().split()]#accepting input
 
     a = [int(x) for x in input().split()]
     member = 0
 
     for i in a:
         if i % m == 0:
-            member += 1
+            member += 1#counting members
 
-    print((2 ** member) - 1)
+    print((2 ** member) - 1)#printing the answer
