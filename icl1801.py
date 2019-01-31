a156989bbda9c87aaa9966b364392bca34706cc0
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-01-31 21:00:31 +0530

    Update icl1801.py

--- icl1801.py
+++ icl1801.py
@@ -1,12 +1,12 @@
-for nums in range(int(input())):
-    n, m = [int(x) for x in input().split()]
+for nums in range(int(input())):#for each test case
+    n, m = [int(x) for x in input().split()]#accept input 
     l = []
     cy, geno = 0, 0
     for j in range(n):
         lis = [int(x) for x in input().split()]
         l += lis
     l.sort()
-    for i in range((n * m) - 1, -1 , -2):
+    for i in range((n * m) - 1, -1 , -2):#reverse loop skipping 1 place
         cy += l[i]
     for i in range((n * m) - 2, -1 , -2):
         geno += l[i]
