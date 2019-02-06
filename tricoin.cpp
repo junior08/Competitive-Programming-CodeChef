3bb4a2fb25d615f07002ba8198dd6c2030b1618f
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-02-06 22:03:53 +0530

    Add files via upload

---
+++ tricoin.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t, n, ans;
+	cin>>t;
+	
+	while(t--){
+		cin>>n;
+		
+		ans = (int)((sqrt(1+8.0*n)-1)/2);
+		cout<<ans<<endl;
+			
+			
+		}
+		
+	}
+
