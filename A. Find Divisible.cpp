0a256f0ea3088a45aae627483c2ed65d044b27d0
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-02-11 02:35:45 +0530

    Add files via upload

---
+++ A. Find Divisible.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	int t, l, r;
+	cin>>t;
+	
+	while(t--)
+	{
+		cin>>l>>r;
+		cout<<l<<" "<<2*l<<"\n";
+	}
+}
