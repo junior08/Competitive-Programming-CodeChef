3c5eb4426cfdb8765e6e3a0cb59287f300540c57
Ayush Tiwari <ayush1rko1tiwari@gmail.com>
2019-02-10 22:13:00 +0530

    Update unicours.py

--- unicours.py
+++ unicours.py
@@ -1,4 +1,4 @@
-for i in range(int(input())):
+for i in range(int(input())):#for number of test cases
     n=int(input())
-    l=[int(x) for x in input().split()]
-    print(len(l)-max(l))
+    l=[int(x) for x in input().split()]#accepting input
+    print(len(l)-max(l))#printing answer 
